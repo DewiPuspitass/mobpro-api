a854b60907d51a1a4a6b7fa9ab6a46be9fc10037
DewiPuspitass <145330132+DewiPuspitass@users.noreply.github.com>
2025-05-25 10:22:22 +0700

    membuat api barang

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Barang;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 
 class BarangController extends Controller
 {
@@ -43,16 +44,14 @@ class BarangController extends Controller
     }
 
     public function store(Request $request){
-        $dataBarang = new Barang;
-
         $rules = [
             'nama_barang' => 'required|string',
-            'jumlah' => 'required|numerik',
-            'harga' => 'required|numerik',
-            'kategori_id' => 'required|numerik',
+            'jumlah' => 'required|numeric',
+            'harga' => 'required|numeric',
+            'kategori_id' => 'required|numeric',
             'barcode' => 'required|string',
             'deskripsi' => 'required|string',
-            'foto_barang' => 'required| string'
+            'foto_barang' => 'required|file|image|mimes:jpeg,png,jpg|max:2048'
         ];
 
         $validator = Validator::make($request->all(), $rules);
@@ -60,18 +59,21 @@ class BarangController extends Controller
         if($validator->fails()){
             return response()->json([
                 'status' => false,
-                'message' => "Gagal melakukan tambah kategori",
+                'message' => "Gagal melakukan tambah barang",
                 'data' => $validator->errors()
             ]);
         }
 
-        $dataBarang->nama_kategori = $request->nama_kategori;
+        $imagePath = $request->file('foto_barang')->store('foto_barang', 'public');
+
+        $dataBarang = new Barang;
+        $dataBarang->nama_barang = $request->nama_barang;
         $dataBarang->jumlah = $request->jumlah;
         $dataBarang->harga = $request->harga;
         $dataBarang->kategori_id = $request->kategori_id;
         $dataBarang->barcode = $request->barcode;
         $dataBarang->deskripsi = $request->deskripsi;
-        $dataBarang->foto_barang = $request->foto_barang;
+        $dataBarang->foto_barang = $imagePath;
 
         $post = $dataBarang->save();
 
@@ -79,10 +81,79 @@ class BarangController extends Controller
             return response()->json([
                 'status' => true,
                 'message' => "Data berhasil ditambahkan",
-                'data' => $post
+                'data' => $dataBarang
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => "Gagal menyimpan data"
             ]);
         }
     }
+
+    public function update(Request $request, $id){
+        $dataBarang = Barang::find($id);
+        
+        if(empty($dataBarang)){
+            return response()->json([
+                'status' => false,
+                'message' => "Barang tidak ditemukan",
+            ], 404);
+        }
+
+        $rules = [
+            'nama_barang' => 'required|string',
+            'jumlah' => 'required|numeric',
+            'harga' => 'required|numeric',
+            'kategori_id' => 'required|numeric',
+            'barcode' => 'required|string',
+            'deskripsi' => 'required|string',
+            'foto_barang' => 'nullable|file|image|mimes:jpeg,png,jpg|max:2048'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => false,
+                'message' => "Gagal melakukan update barang",
+                'data' => $validator->errors()
+            ], 400);
+        }
+
+        
+        if ($request->hasFile('foto_barang')) {
+            if ($dataBarang->foto_barang && Storage::disk('public')->exists($dataBarang->foto_barang)) {
+                Storage::disk('public')->delete($dataBarang->foto_barang);
+            }
+            
+            $imagePath = $request->file('foto_barang')->store('foto_barang', 'public');
+            $dataBarang->foto_barang = $imagePath;
+        }
+
+        $dataBarang->nama_barang = $request->nama_barang;
+        $dataBarang->jumlah = $request->jumlah;
+        $dataBarang->harga = $request->harga;
+        $dataBarang->kategori_id = $request->kategori_id;
+        $dataBarang->barcode = $request->barcode;
+        $dataBarang->deskripsi = $request->deskripsi;
+
+        $post = $dataBarang->save();
+
+        if($post){
+            return response()->json([
+                'status' => true,
+                'message' => "Data berhasil diupdate",
+                'data' => $dataBarang
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => "Gagal update data"
+            ]);
+        }
+    }
+
 
     public function destroy($id){
         $data = Barang::find($id);
@@ -93,6 +164,10 @@ class BarangController extends Controller
                 'message' => "Barang tidak ditemukan"
             ], 404);
         }else{
+            if ($data->foto_barang && Storage::disk('public')->exists($data->foto_barang)) {
+                Storage::disk('public')->delete($data->foto_barang);
+            }
+
             $post = $data->delete();
 
             return response()->json([
