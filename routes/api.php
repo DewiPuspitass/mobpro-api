c3806a5d4dc03140037058632077bd0d760190fd
DewiPuspitass <145330132+DewiPuspitass@users.noreply.github.com>
2025-05-27 15:07:11 +0700

    mengubah nama route

--- routes/api.php
+++ routes/api.php
@@ -19,18 +19,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('kategori', [KategoriController::class, 'index']);
 Route::get('kategori/{id}', [KategoriController::class, 'show']);
-Route::post('kategori/store', [KategoriController::class, 'store']);
+Route::post('kategori/tambah', [KategoriController::class, 'store']);
 Route::put('kategori/{id}', [KategoriController::class, 'update']);
 Route::delete('kategori/{id}', [KategoriController::class, 'destroy']);
 
 Route::get('barang', [BarangController::class, 'index']);
 Route::get('barang/{id}', [BarangController::class, 'show']);
-Route::post('barang/store', [BarangController::class, 'store']);
+Route::post('barang/tambah', [BarangController::class, 'store']);
 Route::put('barang/{id}', [BarangController::class, 'update']);
 Route::delete('barang/{id}', [BarangController::class, 'destroy']);
 
 Route::get('user', [UserController::class, 'index']);
 Route::get('user/{id}', [UserController::class, 'show']);
-Route::post('user/store', [UserController::class, 'store']);
+Route::post('user/tambah', [UserController::class, 'store']);
 Route::put('user/{id}', [UserController::class, 'update']);
 Route::delete('user/{id}', [UserController::class, 'destroy']);
